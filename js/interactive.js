ddc05ee1c588629d74f6ede4030c2d4de58febb2
Ben Cordier <bcordier@columnfivemedia.com>
2014-07-22 17:10:47 -0700

    Version 0.0.2
    Lots of updates, still ahead of schedule.
    Moving towards state based management of UX.
    Tomorrow morning, need to step back and do a little refactoring to make sure that the parent - child object relationships are setup correctly moving forward.
    Next steps, integrate state management with scrolling. Basic architecture / scoping pattern should be as follows:
    
    Interactive
    |___Snap Scroller
    |___Global Collector Obj
    |___State Management
    	|___Scene
    		|___	Subscene
    		|___|___Quiz
    		|___|___Vinvasive
    		|___|___Modals
    		|___|___Scene Collector Instance

--- js/interactive.js
+++ js/interactive.js
@@ -5,37 +5,44 @@ Snap Scroller - Dependencies: jQuery, jQuerys.scrollTo, & Modernizr (optional fo
 */
 
 	var Snapscroller = function (args) {
-		var snap 					= 	this;
-			snap.element 			= 	args.element,
-			snap.parent 			= 	args.parent,
-			snap.easing 			= 	args.easing 			|| 		"linear",
-			snap.duration 			= 	args.duration 			|| 		600,
-			snap.first 				= 	'snapscroller-first',
-			snap.current 			= 	'snapscroller-current',
-			snap.last 				= 	'snapscroller-last',
-			snap.mobile 			= 	(typeof Modernizr !== 'undefined') ? ((Modernizr.touch) ? true : false) : false,
-			snap.scrolling 			= 	false,
+		var snap 					= this;
+			snap.element 			= args.element,
+			snap.parent 			= args.parent,
+			snap.easing 			= args.easing 				|| 		"linear",
+			snap.duration 			= args.duration 			|| 		600,
+			snap.first 				= 'snapscroller-first',
+			snap.current 			= 'snapscroller-current',
+			snap.last 				= 'snapscroller-last',
+			// snap.mobile 			= (typeof Modernizr !== 'undefined') ? ((Modernizr.touch) ? true : false) : false,
+			snap.scrolling 			= false,
+			snap.enabled 			= true,
 			snap.startPosition,
 			snap.delta;
 	};
 
 	//Determines Whether to Bind Scroll Handler to Touch Events or Desktop Scroll
-	Snapscroller.prototype.init  				= function () {
+	// Snapscroller.prototype.init  				= function () {
+	// 	var snap = this;
+	// 	$(snap.element).first().addClass(snap.first).addClass(snap.current);
+	// 	$(snap.element).last().addClass(snap.last);
+	// 	if (snap.mobile === true) {
+	// 		$(snap.element).on("touchstart", function (e) {
+	// 			snap.touchStart(e);
+	// 		});
+	// 		$(snap.element).on("touchmove", function (e) {
+	// 			snap.touchMoveY(e);
+	// 		});
+	// 	} else {
+	// 		$(snap.element).on('mousewheel DOMMouseScroll MozMousePixelScroll wheel', function (e) {
+	// 			snap.scrollDelta(e);
+	// 		});
+	// 	};
+	// };
+
+	Snapscroller.prototype.init 				= function () {
 		var snap = this;
 		$(snap.element).first().addClass(snap.first).addClass(snap.current);
 		$(snap.element).last().addClass(snap.last);
-		if (snap.mobile === true) {
-			$(snap.element).on("touchstart", function (e) {
-				snap.touchStart(e);
-			});
-			$(snap.element).on("touchmove", function (e) {
-				snap.touchMoveY(e);
-			});
-		} else {
-			$(snap.element).on('mousewheel DOMMouseScroll MozMousePixelScroll wheel', function (e) {
-				snap.scrollDelta(e);
-			});
-		};
 	};
 
 	//Set Scroll Delta
@@ -47,7 +54,7 @@ Snap Scroller - Dependencies: jQuery, jQuerys.scrollTo, & Modernizr (optional fo
 		} else if (e.originalEvent.detail) {
 			this.delta = - e.originalEvent.detail / 3;
 		};
-		if (this.delta) {
+		if (this.delta && this.enabled === true) {
 			e.preventDefault ? e.preventDefault() : e.returnValue = false;
 			this.scrollDirectionY();
 		};
@@ -66,7 +73,7 @@ Snap Scroller - Dependencies: jQuery, jQuerys.scrollTo, & Modernizr (optional fo
 		var offset = {};
 		offset.y = this.startPosition.y - e.originalEvent.pageY;
 		this.delta = offset.y;
-		if (Math.abs(this.delta) >= 10) {
+		if (Math.abs(this.delta) >= 10 && this.enabled === true) {
 			e.preventDefault ? e.preventDefault() : e.returnValue = false;
 			this.scrollDirectionY();
 		};
@@ -98,8 +105,8 @@ Snap Scroller - Dependencies: jQuery, jQuerys.scrollTo, & Modernizr (optional fo
 	};
 
 	//Scrolls to Next or Previous Page
-	Snapscroller.prototype.scrollPageY			= function (destination) {
-		var snap = this, id;
+	Snapscroller.prototype.scrollPageY			= function (destination, callback) {
+		var snap = this, id, callback = callback || null;
 		//Next Page
 		if (destination === 'next') {
 			if ($('.' + snap.last).hasClass(snap.current)) {
@@ -108,7 +115,7 @@ Snap Scroller - Dependencies: jQuery, jQuerys.scrollTo, & Modernizr (optional fo
 			} else {
 				$('.' + snap.current).removeClass(snap.current).next().addClass(snap.current);
 				var navTimer = setTimeout(function () {
-					snap.to("." + snap.current, snap.duration);
+					snap.to("." + snap.current, snap.duration, callback);
 				}, snap.duration);
 			};
 		//Previous Page
@@ -119,7 +126,7 @@ Snap Scroller - Dependencies: jQuery, jQuerys.scrollTo, & Modernizr (optional fo
 			} else {
 				$('.' + snap.current).removeClass(snap.current).prev().addClass(snap.current);
 				var navTimer = setTimeout(function () {
-					snap.to("." + snap.current, snap.duration);
+					snap.to("." + snap.current, snap.duration, callback);
 				}, snap.duration);
 			};
 		} else {
@@ -157,6 +164,23 @@ Item Prototype
 /***************************************************************************************************/
 
 /*
+Scene Prototype
+*/
+
+	var Scene 		= function (args) {
+		var scene 				= this;
+			scene.name 			= args.name,
+			scene.index 		= args.index,
+			scene.limit 		= args.limit,
+			scene.scroll 		= args.scroll,
+			scene.collector 	= args.collector,
+			scene.progress 		= args.progress;
+		return this;
+	};
+
+/***************************************************************************************************/
+
+/*
 Progress Bar
 */
 
@@ -172,13 +196,25 @@ Item Collector
 
 	var Collector 	= function (args) {
 		var coll 				= this;
-			coll.array 			= ko.observableArray([]);
+			coll.element 		= args.element,
+			coll.items 			= ko.observableArray([]),
+			coll.all 			= ko.observableArray([]);
+	};
+
+	Collector.prototype.clear 					= function () {
+		var coll = this;
+		for (var i = 0; i < coll.items().length; i++) {
+			coll.all.push(coll.items()[i]);
+		};
+		console.log(coll.all());
+		coll.items([]);
+		console.log(coll.items());
 	};
 
 	Collector.prototype.add 					= function (id) {
 		var coll = this;
-		coll.array.push(id);
-		console.log(coll.array());
+		coll.items.push({item: id});
+		console.log(coll.items());
 	};
 
 /***************************************************************************************************/
@@ -217,24 +253,53 @@ Modal Methods
 /***************************************************************************************************/
 
 /*
+Quiz
+*/
+
+	var Quiz 		= function (args) {
+		var quiz 				= this;
+			quiz.question 		= ko.observable(args.question),
+			quiz.answer 		= ko.observableArray(args.answer);
+	};
+
+	Quiz.prototype.checkResponse 				= function () {
+		var quiz = this;
+	};
+
+/***************************************************************************************************/
+
+/*
 Interactive Parent
 */
 
 	var Interactive = function () {
-		var Int = this;
-			Int.wrapper 	= 	'#interactive-wrapper',
-			Int.slider 		= 	'.viewport',
-			Int.scene 		= 	'.scene',
-			Int.snapScroll 	= 	new Snapscroller({
-				element 	: 	".scene",
-				parent 		: 	".viewport",
-				duration 	: 	600
+		var Int 					= this;
+			Int.wrapper 			= '#interactive-wrapper',
+			Int.slider 				= '.viewport',
+			Int.scene 				= '.scene',
+			Int.subscene			= '.subscene',
+			Int.interativeElement 	= '.interactive-element',
+			Int.imgMask 			= '.img-mask',
+			Int.mask 				= '.mask',
+			Int.snapScroll 			= new Snapscroller({
+				element 			: '.scene',
+				parent 				: '.viewport',
+				duration 			: 600
 			}),
-			Int.collector 	= 	new Collector({
-
+			Int.mobile 				= (typeof Modernizr !== 'undefined') ? ((Modernizr.touch) ? true : false) : false,
+			Int.collector 			= new Collector({
+				element 			: 'item-collector'
 			}),
-			Int.item 		= 	'.item',
-			Int.items 		= 	[],
+			Int.item 				= '.item',
+			Int.itemSelectable 		= '.item.selectable',
+			Int.items 				= {},
+			Int.scenes 				= {},
+			Int.sceneIndex 			= 0,
+			Int.sceneName,
+			Int.sceneLimit,
+			Int.sceneScroll,
+			Int.sceneCollector,
+			Int.sceneProgress,
 			Int.data;
 	};
 
@@ -251,8 +316,24 @@ Methods
 		});
 	};
 
-	//Receives Scene Value as Param and Initializes Items for Scene
-	Interactive.prototype.initItems 			= function (scene) {
+	//Initializes Interactive Scenes
+	Interactive.prototype.initScenes 			= function () {
+		var Int = this;
+		for (var i = 0; i < Int.data.length; i++) {
+			var scene = new Scene({
+				name 		: 	Int.data[i].scene,
+				index 		: 	Int.data[i].index,
+				limit 		: 	Int.data[i].limit,
+				scroll 		: 	Int.data[i].scroll,
+				collector 	: 	Int.data[i].collector,
+				progress 	: 	Int.data[i].progress
+			});
+			Int.scenes[Int.data[i].scene] = scene;
+		};
+	};
+
+	//Initializes Collectable Items
+	Interactive.prototype.initItems 			= function () {
 		var Int = this;
 		for (var i = 0; i < Int.data.length; i++) {
 			for (var j = 0; j < Int.data[i].item.length; j++) {
@@ -261,22 +342,43 @@ Methods
 					quiz 	: 	Int.data[i].item[j].quiz,
 					vin 	: 	Int.data[i].item[j].vin,
 					pest 	: 	Int.data[i].item[j].pest,
-					active 	: 	false	
+					active 	: 	false
 				});
 				Int.items[Int.data[i].item[j].id] = item;
 			}
 		}
 	};
+	//Binds Snap Scroll Events 
+	Interactive.prototype.bindSnapScroll 		= function () {
+		var Int = this;
+		if (Int.mobile === true) {
+			$(Int.snapScroll.element).on("touchstart", function (e) {
+				Int.snapScroll.touchStart(e);
+			});
+			$(Int.snapScroll.element).on("touchmove", function (e) {
+				Int.snapScroll.touchMoveY(e);
+			});
+		} else {
+			$(Int.snapScroll.element).on('mousewheel DOMMouseScroll MozMousePixelScroll wheel', function (e) {
+				Int.snapScroll.scrollDelta(e);
+			});
+		};
+	};
 
 	//Item Selection
 	Interactive.prototype.selectItem 			= function (id) {
 		var Int = this;
+		d3.select('#' + id).classed('selectable', false);
+		Int.setState('fridge');
 		if (Int.items[id].vin !== false) {
 			Int.vinvasive(id);
 		} else if (Int.items[id].quiz !== false) {
-			Int.quiz(id);
+			Int.newQuiz(Int.items[id].quiz);
 		} else {
 			Int.collector.add(id);
+			if (Int.sceneLimit === (Int.collector.items().length + 1)) {
+				Int.sceneEnd();
+			};
 		};
 	};
 
@@ -288,22 +390,72 @@ Methods
 	};
 
 	//Quiz
-	Interactive.prototype.quiz 					= function (id) {
+	Interactive.prototype.newQuiz 				= function (args) {
 		var Int = this;
-		console.log(Int.items[id].quiz);
-		Int.collector.add(id);
+		new Quiz(args);
+	};
+
+	//Set State
+	Interactive.prototype.setState 				= function (state) {
+		var Int = this;
+			$(Int.subscene).css('z-index', 0);
+			$('.' + state).css('z-index', 100);
+			$('.' + state + ' ' + Int.imgMask).fadeIn();
+	};
+
+	//Interactive Scene Start
+	Interactive.prototype.sceneInit 			= function (scene) {
+		var Int = this, scene = Int.scenes[scene];
+			Int.sceneIndex 		= scene.index;
+			Int.sceneScroll 	= scene.scroll;
+			Int.sceneName 		= scene.scene;
+			Int.sceneLimit 		= scene.limit;
+			Int.sceneCollector 	= scene.collector;
+			Int.sceneProgress 	= scene.progress;
+	};
+
+	//Interactive Scene End
+	Interactive.prototype.sceneEnd 				= function (scene) {
+		var Int = this;
+		for (var i = 0; i < Int.data.length; i++) {
+			if (Int.data[i].index === (Int.sceneIndex + 1)) {
+				Int.sceneInit(Int.data[i].scene);
+				Int.nextScene();
+				console.log(Int.data[i].scene);
+			};
+		}
 	};
 
 /*
 Macros
 */
 
+	//Initialize Macro
 	Interactive.prototype.init 					= function () {
 		var Int = this;
 		Int.getData(function () {
 			Int.initItems();
+			Int.initScenes();
 			Int.snapScroll.init();
+			Int.snapScroll.enabled = false;
+			Int.bindSnapScroll();
+			Int.sceneInit('opening');
+			ko.applyBindings(Int.collector, document.getElementById(Int.collector.item));
 		});
+	};
+	//Next Scene Macro
+	Interactive.prototype.nextScene 			= function (scrolling) {
+		var Int = this;
+		Int.snapScroll.enabled = true;
+		if (scrolling !== true) Int.snapScroll.scrollPageY("next", function () {
+			Int.snapScroll.enabled = false;
+		});
+	};
+	//Previous Scene Macro
+	Interactive.prototype.prevScene 			= function (scrolling) {
+		var Int = this;
+		Int.snapScroll.enabled = true;
+		if (scrolling !== true) Int.snapScroll.scrollPageY("prev");
 	};
 
 /*
@@ -317,7 +469,7 @@ Instantiate & Initialize
 Bind Item Events
 */
 
-	$(interactive.wrapper + " " + interactive.item).on("click", function (e) {
+	$(interactive.wrapper + " " + interactive.itemSelectable).on("click", function (e) {
 		var id = e.currentTarget.id;
 		interactive.selectItem(id);
 	});
@@ -333,12 +485,12 @@ Bind Scroll Events
 
 	//Arrow Up & Down Navigation
 	$(interactive.wrapper).on('keydown', function (e) {
-		if (e.keyCode === 40) (e.preventDefault(), interactive.snapScroll.scrollPageY("next"));
-    	if (e.keyCode === 38) (e.preventDefault(), interactive.snapScroll.scrollPageY("prev"));
+		if (e.keyCode === 40) (e.preventDefault(), interactive.nextScene());
+    	if (e.keyCode === 38) (e.preventDefault(), interactive.prevScene());
 	});
 	//Click to Start
 	$('#opening .begin').on("click", function (e) {
-		interactive.snapScroll.scrollPageY("next");
+		interactive.nextScene();
 	});
 
 }(jQuery, d3, ko))
